1e366a8a3fbdc3c56bfb5c5ef98fe782c47e92a4
Michael Tanaka <tanakam4214@gmail.com>
2023-04-28 17:29:10 -0700

    Lab Completed

--- index.js
+++ index.js
@@ -10,14 +10,160 @@
  */
 
 const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  const eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  const payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  console.log(payable);
+  return payable;
+};
 
-    return payable
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+  const employeeRecordObject = {
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return employeeRecordObject;
 }
 
+function createEmployeeRecords(employeesArr) {
+  return employeesArr.map((individualEmployeeArr) =>
+    createEmployeeRecord(individualEmployeeArr)
+  );
+}
+
+function createTimeInEvent(timeInDateAndHour) {
+  const [date, hour] = timeInDateAndHour.split(" ");
+  const timeInEvent = {
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date: date,
+  };
+  this.timeInEvents.push(timeInEvent);
+  return this;
+}
+
+function createTimeOutEvent(timeOutDateAndHour) {
+  const [date, hour] = timeOutDateAndHour.split(" ");
+  const timeOutEvent = {
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date: date,
+  };
+  this.timeOutEvents.push(timeOutEvent);
+  return this;
+}
+
+function hoursWorkedOnDate(soughtDate) {
+  let hoursWorked = 0;
+  const timeInDate = this.timeInEvents.find(
+    (timeIn) => soughtDate === timeIn.date
+  );
+  const timeOutDate = this.timeOutEvents.find(
+    (timeOut) => soughtDate === timeOut.date
+  );
+
+  if (soughtDate && timeInDate.date) {
+    hoursWorked = (timeOutDate.hour - timeInDate.hour) / 100;
+  }
+  return hoursWorked;
+}
+
+function wagesEarnedOnDate(soughtDate) {
+  const hoursWorked = hoursWorkedOnDate.call(this, soughtDate);
+  return hoursWorked * this.payPerHour;
+}
+
+function findEmployeeByFirstName(arrayOfEmployees, firstNameOfSoughtEmployee) {
+  let soughtEmployee = arrayOfEmployees.find((employeeRecordObject) => {
+    if (employeeRecordObject.firstName === firstNameOfSoughtEmployee) {
+      return employeeRecordObject;
+    }
+  });
+  return soughtEmployee;
+}
+
+function calculatePayroll(employeeRecordArray) {
+  let totalWages = employeeRecordArray.reduce((acc, individualRecord) => {
+    return acc + allWagesFor.call(individualRecord);
+  }, 0);
+  return totalWages;
+}
+
+const finalArr = [
+  {
+    firstName: "Thor",
+    familyName: "Odinsson",
+    title: "Electrical Engineer",
+    payPerHour: 45,
+    timeInEvents: [
+      { type: "TimeIn", hour: 8, date: "2018-01-01" },
+      { type: "TimeIn", hour: 8, date: "2018-01-02" },
+      { type: "TimeIn", hour: 8, date: "2018-01-03" },
+    ],
+    timeOutEvents: [
+      { type: "TimeOut", hour: 16, date: "2018-01-01" },
+      { type: "TimeOut", hour: 18, date: "2018-01-02" },
+      { type: "TimeOut", hour: 18, date: "2018-01-03" },
+    ],
+  },
+  {
+    firstName: "Loki",
+    familyName: "Laufeysson-Odinsson",
+    title: "HR Representative",
+    payPerHour: 35,
+    timeInEvents: [
+      { type: "TimeIn", hour: 7, date: "2018-01-01" },
+      { type: "TimeIn", hour: 7, date: "2018-01-02" },
+      { type: "TimeIn", hour: 6, date: "2018-01-03" },
+    ],
+    timeOutEvents: [
+      { type: "TimeOut", hour: 17, date: "2018-01-01" },
+      { type: "TimeOut", hour: 18, date: "2018-01-02" },
+      { type: "TimeOut", hour: 18, date: "2018-01-03" },
+    ],
+  },
+  {
+    firstName: "Natalia",
+    familyName: "Romanov",
+    title: "CEO",
+    payPerHour: 150,
+    timeInEvents: [
+      { type: "TimeIn", hour: 17, date: "2018-01-01" },
+      { type: "TimeIn", hour: 0, date: "2018-01-05" },
+      { type: "TimeIn", hour: 13, date: "2018-01-03" },
+    ],
+    timeOutEvents: [
+      { type: "TimeOut", hour: 23, date: "2018-01-01" },
+      { type: "TimeOut", hour: 23, date: "2018-01-05" },
+      { type: "TimeOut", hour: 23, date: "2018-01-03" },
+    ],
+  },
+  {
+    firstName: "Darcey",
+    familyName: "Lewis",
+    title: "Intern",
+    payPerHour: 15,
+    timeInEvents: [
+      { type: "TimeIn", hour: 7, date: "2018-01-01" },
+      { type: "TimeIn", hour: 8, date: "2018-01-02" },
+      { type: "TimeIn", hour: 8, date: "2018-01-03" },
+    ],
+    timeOutEvents: [
+      { type: "TimeOut", hour: 13, date: "2018-01-01" },
+      { type: "TimeOut", hour: 13, date: "2018-01-02" },
+      { type: "TimeOut", hour: 13, date: "2018-01-03" },
+    ],
+  },
+];
+console.log(calculatePayroll(finalArr));
